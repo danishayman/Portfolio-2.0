bcf7d85885e0f194a71b219c8f714c7b4eb1e600
ayman <danishaiman3b@gmail.com>
2025-04-10 10:00:29 +0800

    Refactor Tailwind configuration: migrate from JavaScript to TypeScript and update content paths and theme settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,15 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     content: [
         './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         extend: {
+            fontFamily: {
+                mono: ['var(--font-roboto-mono)', 'monospace'],
+                rubik: ['var(--font-rubik)', 'sans-serif'],
+            },
             colors: {
                 'theme': 'var(--bt-color)',
                 'btn-text': 'var(--btn-text-color)',
