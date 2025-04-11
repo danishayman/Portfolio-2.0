1ae481b1a6a80a2f599f987a19eb219b3ae8c738
ayman <danishaiman3b@gmail.com>
2025-04-11 11:46:24 +0800

    Added footer section

---
+++ app/component/Footer.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useTheme } from "../common/ThemeContext";
+
+const Footer = () => {
+  const { theme } = useTheme();
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <section 
+      id="footer" 
+      className="py-6 text-center font-mono"
+    >
+      <p className={`text-sm md:text-base ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+        &copy; {currentYear} danishayman.<br />
+        All rights reserved.
+      </p>
+    </section>
+  );
+};
+
+export default Footer; 
