e68bd0d8fad60751ae5c091084bd9296d9ce4983
ayman <danishaiman3b@gmail.com>
2025-04-11 11:48:51 +0800

    Cleaning

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -3,20 +3,20 @@
 import { useTheme } from "../common/ThemeContext";
 
 const Footer = () => {
-  const { theme } = useTheme();
-  const currentYear = new Date().getFullYear();
+    const { theme } = useTheme();
+    const currentYear = new Date().getFullYear();
 
-  return (
-    <section 
-      id="footer" 
-      className="py-6 text-center font-mono"
-    >
-      <p className={`text-sm md:text-base ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
-        &copy; {currentYear} danishayman.<br />
-        All rights reserved.
-      </p>
-    </section>
-  );
+    return (
+        <section
+            id="footer"
+            className="py-6 text-center font-mono"
+        >
+            <p className={`text-sm md:text-base ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+                &copy; {currentYear} danishayman.<br />
+                All rights reserved.
+            </p>
+        </section>
+    );
 };
 
 export default Footer; 
