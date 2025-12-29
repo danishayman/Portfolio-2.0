8407ec3bc13a32b27ee5cc2c19e11767a96bf543
danishayman <danishaiman3b@gmail.com>
2025-12-29 23:15:20 +0800

    add ProjectCard for F-22 Raptor RC Plane to Projects component

--- app/component/Projects.tsx
+++ app/component/Projects.tsx
@@ -11,6 +11,12 @@ export default function Projects() {
 
             <div className="flex flex-col items-center gap-4 pt-4 md:pt-5 lg:pt-8 md:flex-row md:flex-wrap md:justify-center md:gap-6 lg:gap-8">
 
+                <ProjectCard
+                    src="/projects/f22.webp"
+                    link="https://f22-raptor.vercel.app/"
+                    h3="F-22 Raptor RC Plane"
+                    p="DIY Remote Control Plane"
+                />
 
                 <ProjectCard
                     src="/projects/chesayang.webp"
